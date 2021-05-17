1efae41f12586655db9a3d66c7669371a027daca
Wemerson nino <wemersonnino@gmail.com>
2021-05-17 17:46:08 -0300

    send folder build for deploy Netlify

---
+++ build/precache-manifest.73ddff9cd997d7bfe942641b4058cbc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cc1408d0dc29d02c4d3a625e06aa5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91278e636b6d1071084a",
+    "url": "/static/css/main.618940e1.chunk.css"
+  },
+  {
+    "revision": "dca779ace1e7f283b1fe",
+    "url": "/static/js/2.955022e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.955022e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91278e636b6d1071084a",
+    "url": "/static/js/main.6ef94dfe.chunk.js"
+  },
+  {
+    "revision": "4e38718d4f926231304c",
+    "url": "/static/js/runtime-main.9d3565bf.js"
+  }
+]);
